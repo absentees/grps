3e54a096451e3727a33aed17edc0a8498fa2b9df
absentees <scottblissett@gmail.com>
2018-06-01 17:37:46 +1000

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const Xray = require('x-ray');
+const x = Xray({
+	filters: {
+		trimDollar: (value) => {
+			return value.slice(2);
+		}
+	}
+});
+
+app.get('/', (req, res) => {
+	x('https://www.drnks.com/collections/all', '.product', [{
+		title: '.product-title',
+		location: '.product-location',
+		price: '.price',
+		link: '@href'
+	}]).paginate('.page.next-page > a@href').limit(10)(function (err, results) {
+		results.forEach(element => {
+			element.price = parseInt(element.price.trim().substring(1));
+		});
+		results.sort((a, b) => {
+			return a.price - b.price;
+		});
+		res.send(results);
+	});
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
