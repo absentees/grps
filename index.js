30f5bd2eef126ac3d3d04612038c9e28189d3b62
Unknown <scott@presentcompany.co>
2018-06-02 10:10:09 +1000

    Add a frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const expressNunjucks = require('express-nunjucks');
 const cache = require('apicache').middleware;
 const Xray = require('x-ray');
 const x = Xray({
@@ -10,21 +11,45 @@ const x = Xray({
 	}
 });
 
-app.use(cache('1 day'));
+const isDev = app.get('env') === 'development';
 
-app.get('/', (req, res) => {
+app.set('views', __dirname + '/templates');
+
+const njk = expressNunjucks(app, {
+	watch: isDev,
+	noCache: isDev
+});
+
+function getWines(cb) {
 	x('https://www.drnks.com/collections/all', '.product', [{
 		title: '.product-title',
 		location: '.product-location',
 		price: '.price',
-		link: '@href'
-	}]).paginate('.page.next-page > a@href').limit(10)(function (err, results) {
+		link: '@href',
+		imgURL: '.lazy[data-original]@data-original'
+		// Turn on for pagination
+	}]).paginate('.page.next-page > a@href').limit(12)(function (err, results) {
+		// }])(function (err, results) {
 		results.forEach(element => {
 			element.price = parseInt(element.price.trim().substring(1));
 		});
 		results.sort((a, b) => {
 			return a.price - b.price;
-        });
+		});
+		cb(results);
+	});
+}
+
+app.get('/', cache('1 day'), (req, res) => {
+	getWines(winesResults => {
+		res.render('index', {
+			wines: winesResults
+		});
+	});
+});
+
+app.get('/api', cache('1 day'), (req, res) => {
+	getWines(results => {
 		res.send(results);
 	});
 });
